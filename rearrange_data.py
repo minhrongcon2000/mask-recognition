b076a00a34b58e8c4aa15bbc21a63fb7e46e623e
John Pham <john@djavafactory.com>
2022-03-16 19:25:44 +0700

    make rearrange data script reusable

--- rearrange_data.py
+++ rearrange_data.py
@@ -1,8 +1,13 @@
 import os
 import shutil
 from tqdm import tqdm
+import argparse
 
-BASE_DIR = "./dataset/CMFD"
+ap = argparse.ArgumentParser()
+ap.add_argument("--data_dir", type=str)
+args = ap.parse_args()
+
+BASE_DIR = args.data_dir
 
 for folder in tqdm(os.listdir(BASE_DIR)):
     sub_image_dir = os.path.join(BASE_DIR, folder)
