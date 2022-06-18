2269021443e83e3a52687cd3ea9cd162702618be
John Pham <john@djavafactory.com>
2022-06-18 12:28:56 +0700

    Change resolution to 700 x 200

--- detect_mediapipe.py
+++ detect_mediapipe.py
@@ -29,7 +29,7 @@ while True:
     frameCount += 1
     start = time.time()
     frame = vs.read()
-    frame = imutils.resize(frame, width=640, height=320)
+    frame = imutils.resize(frame, width=700, height=200)
     frame_h, frame_w, _ = frame.shape
     
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
