099a4da901d5d417d6bf15d0a583c0c5aba9f6f2
John Pham <john@djavafactory.com>
2022-07-14 14:49:54 +0700

    Change file to test h5

--- detect_face_video_without_nose.py
+++ detect_face_video_without_nose.py
@@ -2,19 +2,18 @@ from imutils.video import VideoStream
 import imutils
 import time
 import cv2
-import os
-import dlib
 import numpy as np
+import mediapipe as mp
 
 from utils import detect_face
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.mobilenet import preprocess_input
-from datetime import datetime
+import math
 
 import config
 
-print("[INFO] loading face detector model...")
-faceNet = cv2.dnn.readNet(config.prototxtPath, config.weightsPath)
+mpFaceDetection = mp.solutions.face_detection
+faceDetection = mpFaceDetection.FaceDetection()
 
 # nose model
 print("[INFO] - loading nose model...")
@@ -29,56 +28,57 @@ print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-captureTime = []
+avgFPS = 0
+frameCount = 0
 
 while True:
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-
-    # num_frames = (num_frames + 1) % save_period
-
-    locs = detect_face(frame, faceNet)
-    
-    faces = []
+    frameCount += 1
     start = time.time()
-    for box in locs:
-        (startX, startY, endX, endY) = box
-        color = (0, 255, 0)
-        cv2.rectangle(frame, (startX, startY),
-                      (endX, endY), (0, 255, 0), 2)
-        face = frame[startY:endY, startX:endX]
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-        face = cv2.resize(face, (224, 224))
-        face = face / 255.0
-        faces.append(face)
-
-    faces = np.array(faces)
-
-    if faces.any():
-        # start = time.time()
-        facePred = faceModel.predict(faces)
-        # end = time.time()
-        # captureTime.append(end - start)
-        for i, (startX, startY, endX, endY) in enumerate(locs):
-            covered, uncovered = facePred[i]
-            label = "incorrect" if uncovered > covered else "correct"
-            percentage = uncovered if uncovered > covered else covered
-            cv2.putText(frame, f"Mask: {label} ({percentage * 100:.2f}%)", (startX - 40, startY - 10),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0) if label == "correct" else (0, 0, 255), 2)
-            cv2.rectangle(frame, (startX, startY),
-                        (endX, endY), (0, 255, 0) if label == "correct" else (0, 0, 255), 2)
-
-            # if num_frames == 0:
-            #     if covered > uncovered:
-            #         cv2.imwrite(os.path.join(
-            #             config.coveredPath, "{}.png".format(datetime.now())), backupFrame)
-            #     else:
-            #         cv2.imwrite(os.path.join(
-            #             config.uncoveredPath, "{}.png".format(datetime.now())), backupFrame)
-            #         print("Send to guard!")
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
+    frame_h, frame_w, _ = frame.shape
+    
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    detection_result = faceDetection.process(frameRGB)
+    
+    locs = []
+    
+    if detection_result.detections:
+        for detection in detection_result.detections:
+            relativeXMin = detection.location_data.relative_bounding_box.xmin
+            relativeYMin = detection.location_data.relative_bounding_box.ymin
+            relativeWidth = detection.location_data.relative_bounding_box.width
+            relativeHeight = detection.location_data.relative_bounding_box.height
+            
+            xMin = min(math.floor(relativeXMin * frame_w), frame_w - 1)
+            yMin = min(math.floor(relativeYMin * frame_h), frame_h - 1)
+            xMax = min(math.floor((relativeXMin + relativeWidth) * frame_w), frame_w - 1)
+            yMax = min(math.floor((relativeYMin + relativeHeight) * frame_h), frame_h - 1)
+            locs.append((xMin, yMin, xMax, yMax))
+            
+            face = frameRGB[yMin:yMax, xMin:xMax]
+            if face.any():
+                face = cv2.resize(face, (224, 224))
+                face = face.astype(np.float32) / 255.0
+                face = np.expand_dims(face, 0)
+            
+                output_data = faceModel.predict(face)
+                classification_result = np.squeeze(output_data)
+                
+                covered, uncovered = classification_result
+                label = "incorrect" if uncovered > covered else "correct"
+                percentage = uncovered if uncovered > covered else covered
+                cv2.putText(frame, f"Mask: {label} ({percentage * 100:.2f}%)", (xMin - 40, yMin - 10),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0) if label == "correct" else (0, 0, 255), 2)
+                cv2.rectangle(frame, (xMin, yMin),
+                                (xMax, yMax), (0, 255, 0) if label == "correct" else (0, 0, 255), 2)
     end = time.time()
-    captureTime.append(end - start)
+    currentFPS = 1 / (end - start + 1e-12) # prevent zero time
+    avgFPS = (1 - 1 / frameCount) * avgFPS + 1 / frameCount * currentFPS
+
     # show the output frame
+    cv2.putText(frame, "Current FPS: " + str(round(currentFPS, 1)), (30, frame_h - 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
+    cv2.putText(frame, "Average FPS: " + str(round(avgFPS, 1)), (30, frame_h - 60), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0))
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
@@ -89,6 +89,6 @@ while True:
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
-print("Number samples: {}".format(len(captureTime)))
-print("Mean time: {}".format(np.mean(captureTime)))
-print("Std: {}".format(np.std(captureTime, ddof=1)))
+# print("Number samples: {}".format(len(captureTime)))
+# print("Mean time: {}".format(np.mean(captureTime)))
+# print("Std: {}".format(np.std(captureTime, ddof=1)))
